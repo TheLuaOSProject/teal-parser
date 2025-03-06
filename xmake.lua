9cd64fd8a07047a3910bddc6895fd2852d5670b2
Amrit Bhogal <ambhogal01@gmail.com>
2025-03-06 18:18:48 +0000

    repo cleanup

--- xmake.lua
+++ xmake.lua
@@ -1,38 +1,13 @@
--- Copyright (C) 2025 Amrit Bhogal
---
--- This program is free software: you can redistribute it and/or modify
--- it under the terms of the GNU Affero General Public License as
--- published by the Free Software Foundation, either version 3 of the
--- License, or (at your option) any later version.
---
--- This program is distributed in the hope that it will be useful,
--- but WITHOUT ANY WARRANTY; without even the implied warranty of
--- MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
--- GNU Affero General Public License for more details.
---
--- You should have received a copy of the GNU Affero General Public License
--- along with this program.  If not, see <https://www.gnu.org/licenses/>.
-
 add_rules("mode.debug", "mode.release")
 
 set_languages("gnuxx23")
 
 target("teal-parser")
     set_kind(is_kind("shared") and "shared" or "static")
-    add_files("teal/**.cpp")
+    add_files("src/**.cpp")
     add_cxxflags(
         "-Wall", "-Wextra", "-Werror",
-        "-Wno-c23-extensions"
+        "-Wno-c23-extensions",
+        "-stdlib=libc++"
     )
 target_end()
-
-target("test")
-    add_files("main.cpp")
-    add_deps("teal-parser")
-    add_cxxflags("-fexperimental-library")
-
-    add_cxxflags(
-        "-Wall", "-Wextra", "-Werror",
-        "-Wno-c23-extensions"
-        -- { force = true }
-    )
