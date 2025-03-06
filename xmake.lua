a850976f9161ac280942877f2abdd2d706adfb0b
Amrit Bhogal <ambhogal01@gmail.com>
2025-03-06 19:44:23 +0000

    cleanup

--- xmake.lua
+++ xmake.lua
@@ -5,6 +5,9 @@ set_languages("gnuxx23")
 target("teal-parser")
     set_kind(is_kind("shared") and "shared" or "static")
     add_files("src/**.cpp")
+    add_headerfiles("src/(teal-parser/**.hpp)", { public = true })
+    add_includedirs("src/", { public = true })
+
     add_cxxflags(
         "-Wall", "-Wextra", "-Werror",
         "-Wno-c23-extensions",
